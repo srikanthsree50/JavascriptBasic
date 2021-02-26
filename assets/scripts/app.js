37084947e91041735a0b09e8083fbc8ad23050c9
srikanthsree50 <srikanthsree50@gmail.com>
2021-02-26 21:59:27 +0530

    calc initial commit

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,34 +1,5 @@
-const backdropElement = document.getElementById('backdrop');
-const modalLinkElements = document.querySelectorAll('.info-modal');
-let infoModal;
-
-function toggleBackdrop() {
-  backdropElement.classList.toggle('visible');
-}
-
-function presentInfoModal(event) {
-  const text = event.target.dataset.text;
-  toggleBackdrop();
-  infoModal = document.createElement('div');
-  infoModal.classList.add('modal');
-  infoModal.innerHTML = `
-    <h2>More Details</h2>
-    <p>${text}</p>
-  `;
-  const closeButton = document.createElement('button');
-  closeButton.addEventListener('click', hideInfoModal);
-  closeButton.textContent = 'Okay';
-  infoModal.appendChild(closeButton);
-  document.body.appendChild(infoModal);
-}
-
-function hideInfoModal() {
-  toggleBackdrop();
-  document.body.removeChild(infoModal);
-}
-
-for (const linkElement of modalLinkElements) {
-  linkElement.addEventListener('click', presentInfoModal);
-}
-
-backdropElement.addEventListener('click', hideInfoModal);
+const defaultResult = 0;
+let currentResult = defaultResult;
+currentResult=(currentResult+20)*3/2-1;
+let calculationDescription = `(${defaultResult} + 10) * 3/2-1`;
+outputResult(currentResult,errorMessage);
