3e03b7b0934b672836bd6525cc8d608f7e8c78ce
srikanthsree50 <srikanthsree50@gmail.com>
2021-02-27 20:33:02 +0530

    calc all functions commit

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,5 +1,52 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
-currentResult=(currentResult+20)*3/2-1;
-let calculationDescription = `(${defaultResult} + 10) * 3/2-1`;
-outputResult(currentResult,errorMessage);
+
+
+function getUserInput(){
+    return parseInt(userInput.value);
+}
+
+function createAndWriteOutput(operator,resultBeforeCalc,calcNumber){
+
+    const calcDescription = ` ${resultBeforeCalc} ${operator} ${calcNumber}`;
+
+    outputResult(currentResult,calcDescription);
+}
+
+function add(){
+    const enteredNumber = getUserInput();
+    const initialResult = currentResult;
+    currentResult = currentResult + enteredNumber;
+    createAndWriteOutput('+',initialResult,enteredNumber);
+}
+
+
+function subtract(){
+    
+    const enteredNumber = getUserInput();
+    const initialResult = currentResult;
+    currentResult = currentResult - enteredNumber;
+    createAndWriteOutput('-',initialResult,enteredNumber);
+}
+
+
+function multiply(){
+    
+    const enteredNumber = getUserInput();
+    const initialResult = currentResult;
+    currentResult = currentResult * enteredNumber;
+    createAndWriteOutput('*',initialResult,enteredNumber);
+}
+
+
+function divide(){
+    
+    const enteredNumber = getUserInput();
+    const initialResult = currentResult;
+    currentResult = currentResult / enteredNumber;
+    createAndWriteOutput('/',initialResult,enteredNumber);
+}
+addBtn.addEventListener('click',add);
+subtractBtn.addEventListener('click',subtract);
+multiplyBtn.addEventListener('click',multiply);
+divideBtn.addEventListener('click',divide);
